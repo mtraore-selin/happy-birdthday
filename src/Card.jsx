a84dc90085f47bf23a6ec58721487064a7844cbd
Mohamed <mohamed@MacBook-Pro.lan>
2024-01-18 02:13:56 +0100

    happy birthday app

---
+++ src/Card.jsx
@@ -0,0 +1,23 @@
+export default () => (
+  <div className="birthdayCard">
+    <div className="cardFront">
+      <h3 className="happy">HAPPY BIRTHDAY Dakawili!</h3>
+      <div className="balloons">
+        <div className="balloonOne" />
+        <div className="balloonTwo" />
+        <div className="balloonThree" />
+        <div className="balloonFour" />
+      </div>
+    </div>
+    <div className="cardInside">
+      <h3 className="back">HAPPY BIRTHDAY !</h3>
+      <p>Dear Sister,</p>
+      <p>
+        Joyeux anniversaire à une personne aussi spéciale que toi ! Que cette
+        journée soit remplie de bonheur, d'amour et de tous les plaisirs que la
+        vie peut offrir. 🎉🎂
+      </p>
+      <p className="name">Aicha</p>
+    </div>
+  </div>
+);
